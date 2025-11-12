91737b9c061de7107d8937e2de0f89d078c64d25
johneleandre03 <beronesjohneleandre03@gmail.com>
2025-11-12 20:18:06 +0800

    added app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+from config import Config
+from models.db import db
+from models.user_model import User
+from flask_migrate import Migrate
+
 import os
 
+load_dotenv()
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# Route para sa login page
+db.init_app(app)
+migrate = Migrate(app, db)
+
 @app.route('/')
-def login():
-    return render_template('login.html')
-
-# Route para sa register page
-@app.route('/register')
-def register():
-    return render_template('register.html')
+def home():
+        return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(port=port, debug=True)
